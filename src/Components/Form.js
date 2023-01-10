e21cbc4b1c6bc91e49e4b8710a8d43d6a126850a
dolyx <86300160+dolyx@users.noreply.github.com>
2023-01-10 11:05:23 +0100

    Add files via upload

---
+++ src/Components/Form.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+render() {
+    return (
+      <form>
+        <label>
+          Titre :
+          <input />
+        </label>
+        <br />
+        <label>
+          Dépense :
+          <input />
+        </label>
+        <label>
+          Date :
+          <input />
+        </label>
+      </form>
+    );
+  }
